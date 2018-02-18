1e3b6280b168be6fb3f2041966d958f7954f20e0
aidabloc <aidabloc@163.com>
2018-02-18 10:15:49 +0800

    2018-02-18

--- algorithm/duvol.py
+++ algorithm/duvol.py
@@ -1,37 +1,33 @@
-"""@author: Young
-@license: (C) Copyright 2013-2018
-@contact: aidabloc@163.com
-@file: average.py
-@time: 2018/1/30 9:54
-"""
 import numpy as np
 
 
-def duvol(x):
-    wa = np.convolve(x, [1. / 60] * 60, mode="full")[59:-59]
-    x = x[59:]
-
-    buffer = []
-    for i in np.arange(60, len(x)):
-        mx = x[i - 60:i]
-        mwa = wa[i - 60:i]
-        mask = mx > mwa
-        up = np.sum(np.power((mx - mwa), 2) * mask) / np.sum(mask)
-        down = np.sum(np.power((mx - mwa), 2) * (1 - mask)) / np.sum(1 - mask)
-        buffer.append(down / up)
-    return np.array([np.nan] * 119 + buffer)
-
-
-def duvol_xx(x, l=60):
+def duvol_down(x):
+    l = 60 # 时间窗口
     wa = np.convolve(x, [1. / l] * l, mode="full")[l - 1:-l + 1]
     x = x[l - 1:]
 
     buffer = []
     for i in np.arange(l, len(x)):
-        mx = x[i - l:i]
-        mwa = wa[i - l:i]
+        mx = x[i - l: i]
+        mwa = wa[i - l: i]
         mask = mx > mwa
-        up = np.sum(np.power((mx - mwa), 2) * mask) / np.sum(mask)
-        down = np.sum(np.power((mx - mwa), 2) * (1 - mask)) / np.sum(1 - mask)
-        buffer.append(down / up)
-    return np.array([np.nan] * (2 * l - 1) + buffer)
+
+        up_mask = np.sum(mask) - 1
+        if not up_mask:
+            up_mask = 1
+        up = np.sum(np.power((mx - mwa), 2) * mask) / up_mask
+
+        down_mask = np.sum(1 - mask) - 1
+        if not down_mask:
+            down_mask = 1
+        down = np.sum(np.power((mx - mwa), 2) * (1 - mask)) / down_mask
+        duvol = down / up
+        buffer.append(duvol)
+    buffer = np.array(buffer)
+    buffer = np.clip(buffer, 0, 100)
+    return np.array([np.nan] * (2 * l - 1) + list(buffer))
+
+
+if __name__ == "__main__":
+    data = np.random.uniform(-0.1, 0.1, size=399)
+    print(duvol(data))
